6fa50b1a1d2d05fa6ecdbbdcd72a30878bec2082
Van Chi Hieu <vanchihieu3@gmail.com>
2024-09-16 21:53:41 +0700

    Isolation trong UnitTest, best practice trong Unit test

--- identity-service/src/main/java/com/devteria/identityservice/configuration/ApplicationInitConfig.java
+++ identity-service/src/main/java/com/devteria/identityservice/configuration/ApplicationInitConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +23,10 @@ public class ApplicationInitConfig {
     PasswordEncoder passwordEncoder;
 
     @Bean
+    @ConditionalOnProperty(prefix = "spring",
+            value = "datasource.driverClassName",
+            havingValue = "com.mysql.cj.jdbc.Driver"
+    )
     ApplicationRunner applicationRunner(UserRepository userRepository) { // hàm này sẽ chạy khi ứng dụng được khởi động
         return args -> {
             if (userRepository.findByUsername("admin").isEmpty()) {
